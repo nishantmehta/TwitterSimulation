a45bd45021cbd5bcaea7cdd31a1c98e7db9fa4e8
nishant <nishantmehta.n@gmail.com>
2014-11-14 14:18:08 -0500

    adding stubs in the user profile class

--- src/UserProfile.scala
+++ src/UserProfile.scala
@@ -1,8 +1,25 @@
 class UserProfile(
     var userID: String,
-    Tweet firstTweet,
-    Tweet LastTweet,
-    Tweet 
+    var LastTweet: Tweet,
+    var listOfFollowing: Map[String, String] = Map(),
+    var numOfTweets: Int
     ) {
-	
+  
+  def getFollowingList() = {
+    //returns a list users followed 
+    listOfFollowing.keySet
+  }
+  
+  def getLastTenTweets() = {
+    //return a list of tweet to the caller
+  }
+  
+  def addTweet(tweet: Tweet) ={
+    // code to add tweet to the end of linked list
+  }
+  
+  def foundMention(userID: String, tweet: Tweet) {
+    // code to add tweet for the user found in the mention
+    //lets do this later
+  }
 }
